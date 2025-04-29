c10c4f4d7cc12435a17bd7dd57d1f427027a94b0
Hugo-24 <hugo.barrerog@udea.edu.co>
2025-04-28 22:35:31 -0500

    Pedir columna al jugador con opción de azar
    Se agrego para pedir las columnas, tambien de forma aleatoria, una variable para ver quien tiene el turno actual, un intento no finalizado para mostrar el ejemplo y ya.

--- proyecto.py
+++ proyecto.py
@@ -1,4 +1,10 @@
 import random
+# Función para imprimir el ejemplo del juego
+def imprimir_ejemplo(): #Todavia no tengo lista la funcion, es intentando entender lo que debo entregar
+    print(" 1234567 ")
+    print("+" + "-" *7 + "+")
+    print(("|" + "." *7 + "|\n") *6, end = "")
+    print("+" + "-" *7 + "+")
 
 # Inicializar el juego
 print("\n")
@@ -29,7 +35,7 @@ print(f"\n{jugador2}, te toca jugar con la siguiente ficha: {ficha2}\n")
 
 # Sorteo de quién inicia la partida
 print("Lanzando una moneda al aire para determinar quién inicia la partida...\n")
-quien_inicia = random.choice([jugador1, jugador2])
+quien_inicia = random.choice([jugador1, jugador2]) #random.choice(lista): elige aleatoriamente un elemento de una lista.
 print(f"La partida la inicia {quien_inicia}\n")
 
 # Mostrar el tablero vacío
@@ -37,5 +43,26 @@ print(" 1234567 ")
 print("+" + "-" *7 + "+")
 print(("|" + "." *7 + "|\n") *6, end = "")
 print("+" + "-" *7 + "+")
+#Ver el turno actual
+turno_actual = quien_inicia
+ficha_actual = ficha1 if quien_inicia == jugador1 else ficha2
 
+# Pedir columna para poner ficha
+columna = ""
+while columna not in ["1", "2", "3", "4", "5", "6", "7", "S"]:
+    columna = input(f"\n{turno_actual}, indica un número de columna o pulsa [S] para tentar a la suerte: ")
+    
+    if columna == "S":
+        columna = str(random.randint(1, 7)) #random.randint(a, b): genera un número entero aleatorio entre 'a' y 'b' incluidos.
+        print(f"\nmmm...suerte con eso, se eligió aleatoriamente la columna {columna} para tu ficha\n")
 
+# Intento pa ver ejemplo
+ejemplo = ""    
+while ejemplo not in ["Y", "N"]:
+    ejemplo = input("\n¿Deseas ver el ejemplo brindado para el proyecto? Ingresa 'Y' para sí o 'N' para no: ")
+    if ejemplo == "Y":
+        imprimir_ejemplo()
+    elif ejemplo == "N":
+        print("\nHas elegido no mostrar el ejemplo.\n")
+    else:
+        print("\nOpción no válida. Continuando sin mostrar el ejemplo.\n")
